1711821b6fbf4b9a4415bb0d4effc82a34dfe57c
Derrick <pshubomi@gmail.com>
2025-09-09 13:55:47 +0100

    - done with the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,95 @@
-import Image from "next/image";
+"use client";
+import AddPostPopup from "@/components/add-post-popup";
+import { useState } from "react";
+export interface Post {
+  postType: string;
+  amountSpent: number;
+  platform?: string;
+  serviceType?: string;
+  reach: number;
+  viewCount: number;
+}
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [serviceType, setServiceType] = useState("pre-service");
+  const [social, setSocial] = useState("instagram");
+  const [isOpen, setIsOpen] = useState(false);
+  const closePopup = () => setIsOpen(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [post, setPost] = useState<Post[]>([
+    {
+      platform: "instagram",
+      serviceType: "pre-service",
+      postType: "service-highlight",
+      viewCount: 1200,
+      reach: 300321,
+      amountSpent: 45000,
+    },
+  ]);
+  // React.useEffect(() => {
+  //   console.log("Service Type changed to:", serviceType);
+  // }, [serviceType]);
+  return (
+    <main className="flex justify-around items-start pt-20 min-h-screen bg-gray-900 px-16 gap-16">
+      <div className="">
+        <h1 className="text-5xl text-white">SPECIAL PROMOTIONAL CAMPAIGN</h1>
+        <h3 className="text-gray-300 text-lg mt-8 mb-4">POST TYPE</h3>
+        <ul className="text-gray-400 list-disc list-inside">
+          <li>Pre-Service</li>
+          <li>In-Service</li>
+          <li>Post-Service</li>
+        </ul>
+      </div>
+      <form className="w-full max-w-3xl border border-gray-100 rounded-md p-4 px-10 pb-20">
+        <h2 className="text-5xl text-gray-500 text-center font-bold mt-4 mb-8">
+          SPC
+        </h2>
+        <label
+          htmlFor="service-type"
+          className="block mb-2 font-medium text-gray-300"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Select Service Type
+        </label>
+        <select
+          id="service-type"
+          name="service-type"
+          value={serviceType}
+          onChange={(e) => setServiceType(e.target.value)}
+          className="mt-1 block bg-gray-600 outline-none w-full py-4 text-white px-4 rounded-md"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <option value="pre-service">Pre Service</option>
+          <option value="in-service">In Service</option>
+          <option value="post-service">Post Service</option>
+        </select>
+        <label
+          htmlFor="service-type"
+          className="block mb-2 font-medium text-gray-300 mt-5"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          Social
+        </label>
+        <select
+          id="social"
+          name="social"
+          value={social}
+          onChange={(e) => setSocial(e.target.value)}
+          className="mt-1 block bg-gray-600 outline-none w-full py-4 text-white px-4 rounded-md"
+        >
+          <option value="instagram">Instagram</option>
+          <option value="x">X</option>
+          <option value="facebook">Facebook</option>
+          <option value="youtube">YouTube</option>
+        </select>
+        <button
+          type="button"
+          className="cursor-pointer mt-10 w-full hover:bg-white bg-black text-white hover:text-black transition-colors duration-500 font-bold py-4 px-4 rounded-md"
+          onClick={() => setIsOpen(true)}
+        >
+          Add Post
+        </button>
+      </form>
+      {isOpen && (
+        <AddPostPopup onClose={closePopup} post={post} setPost={setPost} />
+      )}
+    </main>
   );
 }
---
+++ src/components/add-post-popup.tsx
@@ -0,0 +1,131 @@
+import { Post } from "@/app/page";
+import React from "react";
+import { GrAdd } from "react-icons/gr";
+import { GoIssueClosed } from "react-icons/go";
+import { IoIosClose } from "react-icons/io";
+
+function AddPostPopup({
+  onClose,
+  post,
+  setPost,
+}: {
+  onClose: () => void;
+  post: Post[];
+  setPost: React.Dispatch<React.SetStateAction<Post[]>>;
+}) {
+  const [addPost, setAddPost] = React.useState(false);
+  const [newPost, setNewPost] = React.useState<Post>({
+    postType: "",
+    amountSpent: 0,
+    viewCount: 0,
+    reach: 0,
+  });
+
+  const handleAddPost = () => {
+    setPost([...post, newPost]);
+    setAddPost(false);
+  };
+  return (
+    <div className="fixed inset-0 bg-black/50 flex justify-center items-center">
+      <div className="bg-gray-800 p-8 rounded-md max-w-3xl w-full">
+        <h2 className="text-white text-2xl uppercase">Add Post</h2>
+        <div className="flex flex-wrap gap-4 mt-8 mb-4">
+          {post.map((p, index) => (
+            <div
+              key={index}
+              className="bg-gray-600 text-white p-4 m-2 rounded-md w-full max-w-xs"
+            >
+              <h3 className="text-lg font-bold mb-2">
+                {p.postType.split("-").join(" ").toUpperCase()}
+              </h3>
+              <p className="text-sm">${p.amountSpent}</p>
+            </div>
+          ))}
+        </div>
+        {addPost && (
+          <div className="flex gap-3 items-center">
+            <select
+              id="service-type"
+              name="service-type"
+              value={newPost.postType}
+              onChange={(e) =>
+                setNewPost({ ...newPost, postType: e.target.value })
+              }
+              className="mt-1 block bg-gray-600 outline-none py-4 text-white px-4 rounded-md"
+            >
+              <option value="">-- post type --</option>
+              <option value="service-highlight">Service Highlight</option>
+              <option value="highlight-reel">Highlight Reel</option>
+              <option value="service-flyer">Service Flyer</option>
+              <option value="special-post">Special Post</option>
+              <option value="service-carousel">Service Carousel</option>
+            </select>
+            <div className="flex flex-wrap gap-2">
+              <input
+                type="number"
+                placeholder="View Count"
+                className="mt-1 block border-b-4 border-gray-600 outline-none py-4 text-white px-4 [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none w-28"
+                onChange={(e) =>
+                  setNewPost({ ...newPost, viewCount: Number(e.target.value) })
+                }
+                value={newPost.viewCount || ""}
+              />
+              <input
+                type="number"
+                placeholder="Reach"
+                className="mt-1 block border-b-4 border-gray-600 outline-none py-4 text-white px-4 [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none w-28"
+                onChange={(e) =>
+                  setNewPost({ ...newPost, reach: Number(e.target.value) })
+                }
+                value={newPost.reach || ""}
+              />
+              <input
+                type="number"
+                placeholder="Amount Spent"
+                className="mt-1 block border-b-4 border-gray-600 outline-none py-4 text-white px-4 [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none w-36"
+                onChange={(e) =>
+                  setNewPost({
+                    ...newPost,
+                    amountSpent: Number(e.target.value),
+                  })
+                }
+                value={newPost.amountSpent || ""}
+              />
+            </div>
+
+            <GoIssueClosed
+              className="text-green-500 text-4xl ml-4 cursor-pointer rounded-md block"
+              onClick={handleAddPost}
+            />
+            <IoIosClose
+              className="text-red-500 text-5xl cursor-pointer rounded-md block"
+              onClick={() => setAddPost(false)}
+            />
+          </div>
+        )}
+        {!addPost && (
+          <GrAdd
+            className="text-white border p-1 text-3xl rounded-full ml-4 mt-5 cursor-pointer"
+            onClick={() => setAddPost(true)}
+          />
+        )}
+        <div className="flex justify-between mt-4">
+          <button
+            className="text-white bg-red-400 px-8 py-2 cursor-pointer"
+            onClick={onClose}
+          >
+            Close
+          </button>
+          <button
+            className="text-white bg-blue-500 px-8 py-2 cursor-pointer"
+            onClick={handleAddPost}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddPostPopup;
